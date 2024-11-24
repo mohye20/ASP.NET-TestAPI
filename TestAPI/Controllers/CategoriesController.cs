d38d25f4ddd7aa173918d4f9530ebca721aafc5e
mohye <mohye20@gmail.com>
2024-11-24 15:51:00 +0200

    Create Get MEthod In Web API

---
+++ TestAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestAPI.Data;
+
+namespace TestAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		private readonly AppDbContext _dB;
+
+		public CategoriesController(AppDbContext DB)
+		{
+			_dB = DB;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetCategories()
+		{
+			var Catsegories = await _dB.Categories.ToListAsync();
+			return Ok(Catsegories);
+		}
+	}
+}
