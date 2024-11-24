d89dd55a4545955b90426f3a794aa250313aead5
mohye <mohye20@gmail.com>
2024-11-24 15:37:55 +0200

    Add Db Connection

--- TestAPI/Data/AppDbContext.cs
+++ TestAPI/Data/AppDbContext.cs
@@ -1,6 +1,11 @@
-﻿namespace TestAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestAPI.Data
 {
-	public class AppDbContext
+	public class AppDbContext : DbContext
 	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
 	}
 }
